2cff3407c35175cdf7c73683ad8a05f7562ba154
sunjiaqi <sunjiaqi>
2022-03-07 15:20:40 +0800

    'dog'

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@
  * @Author: sjq
  * @Date: 2022-03-03 10:16:24
  * @LastEditors: sjq
- * @LastEditTime: 2022-03-03 10:27:42
+ * @LastEditTime: 2022-03-07 15:16:19
  */
 import { createRouter, createWebHashHistory } from "vue-router";
 
@@ -18,6 +18,11 @@ const routes = [
     path: "/upload",
     name: "upload",
     component: () => import("@/views/upload.vue"),
+  },
+  {
+    path: "/dog",
+    name: "dog",
+    component: () => import("@/views/dog.vue"),
   },
 ];
 
