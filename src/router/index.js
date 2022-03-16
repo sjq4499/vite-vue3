9db9f8b53d7bc64f03d1e91497a183673e0907ed
sunjiaqi <sunjiaqi>
2022-03-16 14:38:00 +0800

    '整合'

--- src/router/index.js
+++ src/router/index.js
@@ -3,41 +3,67 @@
  * @Author: sjq
  * @Date: 2022-03-03 10:16:24
  * @LastEditors: sjq
- * @LastEditTime: 2022-03-09 16:52:52
+ * @LastEditTime: 2022-03-16 14:31:31
  */
 import { createRouter, createWebHashHistory } from "vue-router";
 
-const routes = [
-  { path: "/", redirect: "/table" },
+export const routes = [
+  { path: "/", redirect: "/upload" },
   {
     path: "/table",
     name: "table",
     component: () => import("@/views/table.vue"),
+    meta: {
+      activeName: "表格",
+    },
   },
   {
     path: "/upload",
     name: "upload",
     component: () => import("@/views/upload.vue"),
+    meta: {
+      activeName: "图片颜色反正",
+    },
   },
   {
     path: "/dog",
     name: "dog",
     component: () => import("@/views/dog.vue"),
+    meta: {
+      activeName: "狗",
+    },
   },
   {
     path: "/scratchCard",
     name: "scratchCard",
     component: () => import("@/views/scratchCard.vue"),
+    meta: {
+      activeName: "瓜瓜乐",
+    },
   },
   {
     path: "/xiaoyi",
     name: "xiaoyi",
     component: () => import("@/views/xiaoyi.vue"),
+    meta: {
+      activeName: "小易",
+    },
   },
   {
-    path: "/aisu",
-    name: "aisu",
-    component: () => import("@/views/aisu.vue"),
+    path: "/susu",
+    name: "susu",
+    component: () => import("@/views/susu.vue"),
+    meta: {
+      activeName: "苏沐儿",
+    },
+  },
+  {
+    path: "/lengr",
+    name: "lengr",
+    component: () => import("@/views/lengr.vue"),
+    meta: {
+      activeName: "冷r",
+    },
   },
 ];
 
---
+++ src/views/lengr.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Descripttion: 
+ * @Author: sjq
+ * @Date: 2022-03-16 14:11:04
+ * @LastEditors: sjq
+ * @LastEditTime: 2022-03-16 14:37:03
+-->
+<template>
+  <div class="">
+    <el-button
+      type="success"
+      class="list"
+      v-for="item in routes"
+      :key="item.name"
+      @click="goPage(item)"
+    >
+      {{ item.meta.activeName }}
+    </el-button>
+  </div>
+</template>
+<script>
+import { defineComponent, ref, reactive } from "vue";
+import { routes } from "@/router/index";
+
+export default defineComponent({
+  setup() {
+    return {
+      routes: routes.filter((item) => item.meta && item.meta.activeName),
+    };
+  },
+  methods: {
+    goPage(data) {
+      this.$router.push(data.path);
+    },
+  },
+});
+</script>
+<style lang="scss" scoped></style>
