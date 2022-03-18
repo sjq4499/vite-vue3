86a7a449dfee49aa82e63b98514414f2256aae92
sjq4499 <51144161+sjq4499@users.noreply.github.com>
2022-03-18 07:27:17 +0000

    Deploying to gh-pages from  @ ba9bac9f376df5de42042e8b3c5a402054adf05b 🚀

---
+++ assets/localStorage.3eb338c9.js
@@ -0,0 +1,1 @@
+function o(e){return window.localStorage.getItem(e)}function r(e,a){return window.localStorage.setItem(e,a)}function l(e){return window.localStorage.removeItem(e)}export{o as g,l as r,r as s};
