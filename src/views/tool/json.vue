0be111ba229ed0500891be69d6f0959f549084e6
sunjiaqi <sunjiaqi>
2022-04-14 10:14:27 +0800

    'json'

---
+++ src/views/tool/json.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Descripttion: 
+ * @Author: sjq
+ * @Date: 2022-04-14 10:09:47
+ * @LastEditors: sjq
+ * @LastEditTime: 2022-04-14 10:09:47
+-->
+<template>
+  <div class="">json</div>
+</template>
+<script>
+import { defineComponent, ref, reactive } from "vue";
+export default defineComponent({
+  name: "Json",
+  setup() {
+    return;
+  },
+});
+</script>
+<style lang="scss" scoped></style>
