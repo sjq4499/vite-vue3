2286baab3f7e1c33892b54e1fbbcfc361c8c10cb
sunjiaqi <sunjiaqi>
2022-04-14 15:15:04 +0800

    '二维码'

---
+++ src/views/tool/qrcode.vue
@@ -0,0 +1,68 @@
+<!--
+ * @Descripttion: 
+ * @Author: sjq
+ * @Date: 2022-04-14 14:06:14
+ * @LastEditors: sjq
+ * @LastEditTime: 2022-04-14 15:14:25
+-->
+<template>
+  <div>内容： <el-input v-model="value" placeholder=""></el-input></div>
+  <div class="qrcode">
+    <canvas id="canvas"></canvas>
+  </div>
+</template>
+<script>
+import { defineComponent, ref, reactive, onMounted, watchEffect } from "vue";
+import Qrcode from "qrcode";
+
+export default defineComponent({
+  name: "Qrcode",
+  setup() {
+    const qrcodeUrl = ref("200");
+    const value = ref("内容");
+    const qrOptions = reactive({
+      // errorCorrectionLevel: "H", //纠错级别 L, M, Q, H
+      // type: "image/jpeg",//数据 URI 格式。可能的值为：image/png, image/jpeg, image/webp。
+      // quality: 0.3, //图像质量
+      // margin: 1,
+      color: {
+        // 必须是rgba
+        // dark: "#00000000", //前背景
+        // light: "#f5f5f5f5", //背景色
+      },
+    });
+    const qrCode = (url) => {
+      console.log(qrOptions);
+      let canvas = document.getElementById("canvas");
+      Qrcode.toCanvas(canvas, url || value.value, qrOptions, function (error) {
+        if (error) console.error(error);
+      });
+    };
+    onMounted(() => {
+      qrCode();
+    });
+    watchEffect(() => {
+      if (qrCode) qrCode();
+    });
+
+    return {
+      qrcodeUrl,
+      value,
+      qrCode,
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+.qrcode {
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+  text-align: center;
+  #canvas {
+    display: block;
+    width: 100% !important;
+    height: 100% !important;
+  }
+}
+</style>
