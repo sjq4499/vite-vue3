a634a0f4a8bc4bbf04c31faaa002415f30b8d09c
sunjiaqi <sunjiaqi>
2022-06-15 18:34:46 +0800

    '扫雷'

--- src/views/tool/mineGame.vue
+++ src/views/tool/mineGame.vue
@@ -3,11 +3,17 @@
     <div class="list" v-for="(item, index) in buttonArr" :key="index">
       <button
         v-for="(ite, ind) in item"
-        :key="ind"
-        :class="{ active: ite.num === 10 && isShow, bgc1: isShow }"
-        @click="handClick(index, ind)"
+        :key="index + '' + ind"
+        :class="{
+          active: ite.num === 10 && ite.isShow,
+          bgc1: ite.isShow,
+          bgc3: ite.isSign,
+        }"
+        @click="handClick(ite, index, ind)"
+        @contextmenu.prevent="signClick(ite, index, ind)"
       >
-        <span v-if="ite.num > 0 && isShow">{{ ite.num }}</span>
+        <span v-if="ite.num > 0">{{ ite.num }}</span>
+        <!-- <span v-if="ite.num > 0 && ite.isShow">{{ ite.num }}</span> -->
       </button>
     </div>
   </div>
@@ -18,10 +24,11 @@ export default defineComponent({
   name: "mineGame",
 
   setup() {
-    let buttonArr = reactive([]);
-    let getNumArr = reactive([]);
     let w = ref(16);
-
+    let buttonArr = ref([]);
+    let checkArr = ref([]);
+    let index = ref(0);
+    let leiNumber = ref(20);
     //生成地雷
     const renderLei = () => {
       const lei = () => {
@@ -31,13 +38,13 @@ export default defineComponent({
           const x = Math.floor(Math.random() * 16);
           const y = Math.floor(Math.random() * 16);
           leiArr.push([y, x]);
-          if (leiArr.length == 40) {
+          if (leiArr.length == leiNumber.value) {
             //数组去重
             let obj = {};
             leiArr.forEach((item) => (obj[item] = item));
             leiArr = Object.values(obj);
           }
-          if (leiArr.length == 40) {
+          if (leiArr.length == leiNumber.value) {
             return;
           }
           fn();
@@ -48,60 +55,42 @@ export default defineComponent({
       let leiArr = lei();
       //渲染地雷
       leiArr.forEach((item) => {
-        buttonArr[item[0]][item[1]].num = 10;
+        buttonArr.value[item[0]][item[1]].num = 10;
       });
     };
     //获取格子周围一圈格子的函数
     const getBox = (x, y) => {
       //第一排的格子只需要选中前两排
-      if (y == 0) {
-        for (let i = 0; i < 2; i++) {
-          //如果两个格子x坐标相减的绝对值小于或等于1，那么这两个格子就是相邻的
-          let getNumArr02 = buttonArr[i].filter(
-            (item, index) => Math.abs(index - x) <= 1
-          );
-          //加入数组
-          getNumArr.push(...getNumArr02);
-        }
+      let getNumArr = [];
+      let minx = x - 1 >= 0 ? x - 1 : 0;
+      let maxx = x + 1 > w.value - 1 ? w.value - 1 : x + 1;
+      for (let i = minx; i <= maxx; i++) {
+        //如果两个格子y坐标相减的绝对值小于或等于1，那么这两个格子就是相邻的
+        let getNumArr02 = buttonArr.value[i].filter(
+          (item, index) => Math.abs(item.y - y) <= 1
+        );
+        //加入数组
+        getNumArr.push(...getNumArr02);
       }
-      //第二排至倒数第二排，需要选中自身上中下三排的格子
-      else if (y >= 1 && y < w.value - 1) {
-        for (let i = +y - 1; i < +y + 2; i++) {
-          let getNumArr02 = buttonArr[i].filter(
-            (item, index) => Math.abs(index - x) <= 1
-          );
-          getNumArr.push(...getNumArr02);
-        }
-      } else {
-        //最后一排，选中两排的格子遍历
-        for (let i = w.value - 2; i < w.value; i++) {
-          let getNumArr02 = buttonArr[i].filter(
-            (item, index) => Math.abs(index - x) <= 1
-          );
-          getNumArr.push(...getNumArr02);
-        }
-      }
+      return getNumArr;
     };
     //获取格子数字函数
     const getNum = (x, y) => {
       //建立一个存放目标格子周围一圈格子的数组
-      getNumArr = [];
-      // const y = ul.dataset.y;
-      // const x = btn.dataset.x;
       //调用获取格子周围一圈格子的函数，将格子的坐标传入参数
-      getBox(x, y);
+      let getNumArr = getBox(x, y);
       //遍历这个格子周围一圈的格子，有地雷的话num就+1
-      getNumArr.forEach((item1) => {
-        if (item1.num == 10) {
-          buttonArr[x][y].num++;
+      getNumArr.forEach((item) => {
+        if (item.num == 10) {
+          buttonArr.value[x][y].num++;
         }
       });
     };
     //渲染格子数字
     const renderNum = () => {
-      buttonArr.forEach((item, i) => {
+      buttonArr.value.forEach((item, i) => {
         item.forEach((item02, j) => {
-          if (item02.num == 0) {
+          if (item02.num !== 10) {
             //获取不是地雷的标签
             // let ul = document.querySelectorAll(".list")[i];
             // let btn = ul.querySelectorAll("button")[i02];
@@ -123,39 +112,113 @@ export default defineComponent({
     };
     // 初始化
     const init = () => {
-      buttonArr = new Array(w.value);
+      buttonArr.value = new Array(w.value);
+      let index = 1;
       for (let i = 0; i < w.value; i++) {
-        buttonArr[i] = new Array(w.value);
+        buttonArr.value[i] = new Array(w.value);
         for (let j = 0; j < w.value; j++) {
-          buttonArr[i][j] = { num: 0, isShow: false };
+          buttonArr.value[i][j] = {
+            num: 0,
+            isShow: false,
+            x: i,
+            y: j,
+            index: index++,
+            isSign: false,
+          };
         }
       }
       renderLei();
       renderNum();
     };
-    const handClick = (x, y) => {
-      let arr = JSON.parse(JSON.stringify(buttonArr));
-      if (buttonArr[x][y].num === 10) {
-        arr[x][y].isShow = true;
+    // 显示周围的白色格子
+    const showWhiteGrid = (x, y) => {
+      //空白格子的周围一圈一定没有地雷，直接让这些格子显示类容
+      //再次调用获取周围一圈格子的函数
+      let getNumArr = getBox(x, y);
+      let arr = [];
+      getNumArr.forEach((item, index) => {
+        //点击空白格，就会自动把周围一圈的格子都显示
+        if (item.num != 10) {
+          if (
+            !buttonArr.value[item.x][item.y].isShow &&
+            !buttonArr.value[item.x][item.y].isSign
+          ) {
+            buttonArr.value[item.x][item.y].isShow = true;
+          }
+          if (
+            buttonArr.value[item.x][item.y].num == 0 &&
+            !buttonArr.value[item.x][item.y].isSign &&
+            checkArr.value.findIndex((check) => check.index === item.index) ===
+              -1
+          ) {
+            //如果空白格周围一圈格子里面还有空白格，就将他们加入这个num0数组中，稍后再次循环一次这个点击事件
+            arr.push(item);
+          }
+        }
+      });
+      // console.log(arr, "llla");
+      arr.forEach((item) => {
+        handClick(item, item.x, item.y);
+      });
+    };
+    const handClick = (data, x, y) => {
+      // getNum(x, y);
+      console.log(
+        checkArr.value.findIndex((check) => check.index === data.index),
+        "index"
+      );
+      console.log(data);
+
+      if (
+        checkArr.value.findIndex((check) => check.index === data.index) !== -1
+      )
+        return;
+
+      index.value++;
+      checkArr.value.push(data);
+      if (buttonArr.value[x][y].num === 10) {
+        buttonArr.value[x][y].isShow = true;
         // setTimeout(function () {
         //   alert("游戏失败");
         //   init();
         // }, 200);
       } else {
-        arr[x][y].isShow = true;
+        if (!buttonArr.value[x][y].isShow) {
+          buttonArr.value[x][y].isShow = true;
+        }
+        //判断如果点击的是空白格子
+        if (buttonArr.value[x][y].num === 0) {
+          showWhiteGrid(x, y);
+        }
       }
-      buttonArr = JSON.parse(JSON.stringify(arr));
-
-      console.log(buttonArr[x][y], buttonArr[x][y].isShow, buttonArr);
+    };
+    const signClick = (data, x, y) => {
+      if (buttonArr.value[x][y].isShow) return;
+      checkArr.value.push(data);
+      console.log(data);
+      if (buttonArr.value[x][y].isSign) {
+        buttonArr.value[x][y].isSign = false;
+        let index = checkArr.value.findIndex(
+          (item) => item.index === data.index
+        );
+        console.log(checkArr.value.length, "1");
+        checkArr.value.splice(index, 1);
+        // checkArr.value = [];
+        console.log(checkArr.value.length, "2");
+      } else {
+        buttonArr.value[x][y].isSign = true;
+      }
     };
 
     init();
 
     return {
       buttonArr,
-      getNumArr,
       w,
+      index,
+      checkArr,
       handClick,
+      signClick,
     };
   },
 });
@@ -188,13 +251,13 @@ button {
 }
 
 .bgc3 {
-  background: url(./qq.jpg) no-repeat;
+  background: url(@/assets/sign-icon.png) no-repeat;
   background-color: #c0c0c0;
   background-size: 100%;
 }
 
 .active {
-  background: url(./Snipaste_2022-06-12_16-24-48.jpg) no-repeat;
+  background: url(@/assets/mine-icon.png) no-repeat;
   background-size: 100%;
 }
 
