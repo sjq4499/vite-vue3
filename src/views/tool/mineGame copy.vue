6052bcb7eae8edeb9d81409e637587caf5404e93
sunjiaqi <sunjiaqi>
2022-06-22 17:03:18 +0800

    'vue2扫雷'

--- src/views/tool/mineGame copy.vue
+++ src/views/tool/mineGame copy.vue
@@ -1,20 +1,59 @@
 <!--
  * @Descripttion: 
  * @Author: sjq
- * @Date: 2022-06-14 09:53:04
+ * @Date: 2022-06-22 10:49:45
  * @LastEditors: sjq
- * @LastEditTime: 2022-06-14 18:08:08
+ * @LastEditTime: 2022-06-22 14:22:19
 -->
 <template>
-  <div class="box">
-    <div class="list" v-for="(item, index) in buttonArr" :key="index">
-      <button
-        v-for="(ite, ind) in item"
-        :key="ind"
-        :class="{ active: ite.num === 10 }"
+  <div class="">
+    <div class="mine_header">
+      <span>格子：{{ w }}*{{ w }}</span>
+      <span>雷：{{ leiNumber }} </span>
+      <span>标记：{{ signNum }}</span>
+    </div>
+    <div class="box">
+      <div class="list" v-for="(item, index) in buttonArr" :key="index">
+        <div
+          v-for="(ite, ind) in item"
+          class="list_row"
+          :key="ite.index"
+          :class="{
+            active: ite.num === 10 && ite.isShow,
+            bgc1: ite.isShow,
+            bgc3: ite.isSign,
+          }"
+          @click="handClick(ite, index, ind)"
+        >
+          <!-- @contextmenu.prevent="signClick(ite, index, ind)" -->
+          <!-- <span v-if="ite.num > 0" :class="'lei' + ite.num">{{ ite.num }}</span> -->
+          <span
+            v-if="ite.num !== 10 && ite.num > 0 && ite.isShow"
+            :class="'lei' + ite.num"
+            >{{ ite.num }}</span
+          >
+        </div>
+      </div>
+      <div v-if="gameProgress > 1" class="progress">
+        <div class="succed" v-if="gameProgress === 2">游戏胜利！</div>
+        <div class="error" v-else-if="gameProgress === 3">游戏失败！</div>
+      </div>
+    </div>
+    <div class="fn_box">
+      <div
+        @click="changeFn(1)"
+        :class="{ fn_active: fnActiveName === 1 }"
+        class="fn"
       >
-        <span v-if="ite.num > 0">{{ ite.num }}</span>
-      </button>
+        排雷
+      </div>
+      <div
+        @click="changeFn(2)"
+        :class="{ fn_active: fnActiveName === 2 }"
+        class="fn"
+      >
+        插旗
+      </div>
     </div>
   </div>
 </template>
@@ -25,45 +64,45 @@ export default {
   data() {
     return {
       buttonArr: [],
-      getNumArr: [],
-      w: 16,
+      w: 10,
+      index: 0,
+      checkArr: [],
+      signNum: 0,
+      leiNumber: 10,
+      gameProgress: 0, //0未开始 1进行中 2成功 3失败
+      fnActiveName: 1, //1排雷 2插旗
     };
   },
   computed: {},
-  watch: {},
+  watch: {
+    checkArr() {
+      if (Math.pow(this.w, 2) === this.checkArr.length) {
+        this.gameProgress = 2;
+      }
+    },
+  },
   created() {
     this.init();
   },
   mounted() {},
   methods: {
-    init() {
-      let buttonArr = new Array(this.w);
-      for (let i = 0; i < this.w; i++) {
-        buttonArr[i] = new Array(this.w);
-        for (let j = 0; j < this.w; j++) {
-          buttonArr[i][j] = { num: 0 };
-        }
-      }
-      this.buttonArr = buttonArr;
-      this.renderLei();
-      this.renderNum();
-    },
     //生成地雷
     renderLei() {
       const lei = () => {
         let leiArr = [];
+        let num = this.leiNumber;
         function fn() {
           //获取随机坐标
-          const x = Math.floor(Math.random() * 16);
-          const y = Math.floor(Math.random() * 16);
+          const x = Math.floor(Math.random() * num);
+          const y = Math.floor(Math.random() * num);
           leiArr.push([y, x]);
-          if (leiArr.length == 40) {
+          if (leiArr.length == num) {
             //数组去重
             let obj = {};
             leiArr.forEach((item) => (obj[item] = item));
             leiArr = Object.values(obj);
           }
-          if (leiArr.length == 40) {
+          if (leiArr.length == num) {
             return;
           }
           fn();
@@ -77,197 +116,178 @@ export default {
         this.buttonArr[item[0]][item[1]].num = 10;
       });
     },
-    renderDiv() {
-      document.querySelector("div").innerHTML = "";
-      buttonArr.forEach((item, index) => {
-        let ul = document.createElement(".list");
-        //给ul标签添加自定义属性y
-        ul.dataset.y = index;
-        item.forEach((item2, index2) => {
-          let button = document.createElement("button"); //遍历数组，绘制棋盘
-          //给button标签添加自定义属性x，用来作为坐标使用
-          button.dataset.x = index2;
-          if (item2.num === 10) {
-            //给地雷元素添加一个自定义属性，便于识别
-            button.dataset.z = 10;
-            //写的时候可以把地雷先渲染出来，写完了再注释掉
-            // button.classList.add('active')
-          } else {
-            item2.num = 0;
-          }
-          ul.appendChild(button);
-        });
-        document.querySelector("div").appendChild(ul);
+    //获取格子周围一圈格子的函数
+    getBox(x, y) {
+      //第一排的格子只需要选中前两排
+      let getNumArr = [];
+      let minx = x - 1 >= 0 ? x - 1 : 0;
+      let maxx = x + 1 > this.w - 1 ? this.w - 1 : x + 1;
+      for (let i = minx; i <= maxx; i++) {
+        //如果两个格子y坐标相减的绝对值小于或等于1，那么这两个格子就是相邻的
+        let getNumArr02 = this.buttonArr[i].filter(
+          (item, index) => Math.abs(item.y - y) <= 1
+        );
+        //加入数组
+        getNumArr.push(...getNumArr02);
+      }
+      return getNumArr;
+    },
+    //获取格子数字函数
+    getNum(x, y) {
+      //建立一个存放目标格子周围一圈格子的数组
+      //调用获取格子周围一圈格子的函数，将格子的坐标传入参数
+      let getNumArr = this.getBox(x, y);
+      //遍历这个格子周围一圈的格子，有地雷的话num就+1
+      getNumArr.forEach((item) => {
+        if (item.num == 10) {
+          this.buttonArr[x][y].num++;
+        }
       });
     },
     //渲染格子数字
     renderNum() {
       this.buttonArr.forEach((item, i) => {
         item.forEach((item02, j) => {
-          if (item02.num == 0) {
-            //获取不是地雷的标签
-            // let ul = document.querySelectorAll(".list")[i];
-            // let btn = ul.querySelectorAll("button")[i02];
-            //调用获取格子数字的函数，传入3个参数，当前格子对应的数组元素，当前行，和当前格子
+          if (item02.num !== 10) {
             this.getNum(i, j);
-            //判断这个格子是否带有数字
-            // if (item02.num < 10 && item02.num > 0) {
-            //   //给有数字的格子添加一个自定义属性，便于识别
-            //   btn.dataset.z = 1;
-            //   const span = document.createElement("span");
-            //   //将数字渲染到格子中
-            //   span.innerText = item02.num;
-            //   span.style.display = "none";
-            //   btn.appendChild(span);
-            // }
           }
         });
       });
     },
-    ////获取格子数字函数
-    getNum(x, y) {
-      //建立一个存放目标格子周围一圈格子的数组
-      this.getNumArr = [];
-      // const y = ul.dataset.y;
-      // const x = btn.dataset.x;
-      //调用获取格子周围一圈格子的函数，将格子的坐标传入参数
-      this.getBox(x, y);
-      //遍历这个格子周围一圈的格子，有地雷的话num就+1
-      this.getNumArr.forEach((item1) => {
-        if (item1.num == 10) {
-          this.buttonArr[x][y].num++;
-        }
-      });
-    },
-    //获取格子周围一圈格子的函数
-    getBox(x, y) {
-      //第一排的格子只需要选中前两排
-      if (y == 0) {
-        this.buttonArr[x + 1][y];
-        this.buttonArr[x - 1][y];
-        this.buttonArr[x][y + 1];
-        for (let i = 0; i < 2; i++) {
-          //如果两个格子x坐标相减的绝对值小于或等于1，那么这两个格子就是相邻的
-          let getNumArr02 = this.buttonArr[i].filter(
-            (item, index) => Math.abs(index - x) <= 1
-          );
-          //加入数组
-          this.getNumArr.push(...getNumArr02);
+    // 初始化
+    init() {
+      this.buttonArr = new Array(this.w);
+      let index = 1;
+      for (let i = 0; i < this.w; i++) {
+        this.buttonArr[i] = new Array(this.w);
+        for (let j = 0; j < this.w; j++) {
+          this.buttonArr[i][j] = {
+            num: 0,
+            isShow: false,
+            x: i,
+            y: j,
+            index: index++,
+            isSign: false,
+          };
         }
       }
-      //第二排至倒数第二排，需要选中自身上中下三排的格子
-      else if (y >= 1 && y < this.w - 1) {
-        for (let i = +y - 1; i < +y + 2; i++) {
-          let getNumArr02 = this.buttonArr[i].filter(
-            (item, index) => Math.abs(index - x) <= 1
-          );
-          getNumArr.push(...getNumArr02);
+      this.renderLei();
+      this.renderNum();
+    },
+    // 显示周围的白色格子
+    showWhiteGrid(x, y) {
+      //空白格子的周围一圈一定没有地雷，直接让这些格子显示类容
+      //再次调用获取周围一圈格子的函数
+      let getNumArr = this.getBox(x, y);
+      let arr = [];
+      getNumArr.forEach((item, index) => {
+        //点击空白格，就会自动把周围一圈的格子都显示
+        if (item.num != 10) {
+          if (
+            !this.buttonArr[item.x][item.y].isShow &&
+            !this.buttonArr[item.x][item.y].isSign
+          ) {
+            this.buttonArr[item.x][item.y].isShow = true;
+          }
+          if (
+            this.buttonArr[item.x][item.y].num == 0 &&
+            !this.buttonArr[item.x][item.y].isSign &&
+            this.checkArr.findIndex((check) => check.index === item.index) ===
+              -1
+          ) {
+            //如果空白格周围一圈格子里面还有空白格，就将他们加入这个num0数组中，稍后再次循环一次这个点击事件
+            arr.push(item);
+          } else if (
+            this.checkArr.findIndex((check) => check.index === item.index) ===
+            -1
+          ) {
+            this.checkArr.push(item);
+          }
         }
+      });
+      // console.log(arr, "llla");
+      arr.forEach((item) => {
+        this.handClick(item, item.x, item.y);
+      });
+    },
+    // 排雷
+    handClick(data, x, y) {
+      this.gameProgress = 1;
+      if (this.fnActiveName === 2) {
+        this.signClick(data, x, y);
+        return;
+      }
+      // getNum(x, y);
+      if (this.checkArr.findIndex((check) => check.index === data.index) !== -1)
+        return;
+
+      this.index++;
+      this.checkArr.push(data);
+      if (this.buttonArr[x][y].num === 10) {
+        this.buttonArr[x][y].isShow = true;
+        this.gameProgress = 3;
+        // setTimeout(function () {
+        //   alert("游戏失败");
+        //   init();
+        // }, 200);
       } else {
-        //最后一排，选中两排的格子遍历
-        for (let i = this.w - 2; i < this.w; i++) {
-          const allButton03 = allUl[i].querySelectorAll("button");
-          let getNumArr02 = Array.from(allButton03).filter(
-            (item, index) => Math.abs(index - x) <= 1
-          );
-          getNumArr.push(...getNumArr02);
+        if (!this.buttonArr[x][y].isShow) {
+          this.buttonArr[x][y].isShow = true;
+        }
+        //判断如果点击的是空白格子
+        if (this.buttonArr[x][y].num === 0) {
+          this.showWhiteGrid(x, y);
         }
       }
     },
-    handClick() {
-      allUl.forEach((buttons) => {
-        buttons.querySelectorAll("button").forEach((item) => {
-          item.addEventListener("click", function () {
-            //点击效果
-            this.classList.add("bgc1");
-            //如果这个格子有数字，就显示
-            if (this.querySelector("span")) {
-              this.querySelector("span").style.display = "block";
-            }
-            allArr.push(this);
-            //判断，如果点击的是地雷，游戏结束
-            if (item.dataset.z == 10) {
-              item.classList.add("active");
-              setTimeout(function () {
-                alert("游戏失败");
-                location.reload();
-              }, 200);
-            }
-            //只有空白的格子没有自定义的z属性，判断如果点击的是空白格子
-            if (!item.dataset.z) {
-              const num0Arr = [];
-              num0(item);
-              //空白格子的周围一圈一定没有地雷，直接让这些格子显示类容
-              function num0(item) {
-                getNumArr = [];
-                const buttons = item.parentNode;
-                const y = buttons.dataset.y;
-                const x = item.dataset.x;
-                //再次调用获取周围一圈格子的函数
-                getBox(x, y);
-                getNumArr.forEach((itemBtn) => {
-                  //点击空白格，就会自动把周围一圈的格子都显示
-                  if (itemBtn.dataset.z != 10) {
-                    itemBtn.classList.add("bgc1");
-                  }
-                  if (itemBtn.querySelector("span")) {
-                    itemBtn.querySelector("span").style.display = "block";
-                  }
-                  //将这些格子都加入总数组中
-                  allArr.push(itemBtn);
-                  if (!itemBtn.dataset.z) {
-                    //如果空白格周围一圈格子里面还有空白格，就将他们加入这个num0数组中，稍后再次循环一次这个点击事件
-                    num0Arr.push(itemBtn);
-                  }
-                });
-              }
-              //给空白格周围的空白格也添加一个显示类容的函数
-              function clickNum0() {
-                //num0Arr包含了点击的空白格周围9个格子内的所有空白格子，newNum0Arr就是除掉自身的所有空白格子
-                const newNum0Arr = num0Arr.filter((item2) => item2 != item);
-                newNum0Arr.forEach((item02) => {
-                  //再次在其他空白格身上调用显示周围一圈内容的函数，这样就形成点击一个空白格子，
-                  //如果这个格子周围空白区域很多，能显示一大片区域的效果。
-                  num0(item02);
-                });
-              }
-              clickNum0();
-            }
-            //给总数组去重
-            const newAllArr = [...new Set(allArr)];
-            //筛选，排除是地雷的格子
-            const newAllArr02 = newAllArr.filter(
-              (item) => item.dataset.z != 10
-            );
-            //一共256个格子，40个地雷，如果数组长度达到216，就可以判定胜利了。
-            if (newAllArr02.length == 216) {
-              alert("游戏胜利！");
-              location.reload();
-            }
-          });
-          //鼠标右键点击事件，用来插棋子
-          item.addEventListener("contextmenu", function () {
-            if (
-              !this.classList.contains("bgc3") &&
-              !this.classList.contains("bgc1")
-            ) {
-              this.classList.add("bgc3");
-            } else {
-              this.classList.remove("bgc3");
-            }
-          });
-        });
-      });
+    // 标雷
+    signClick(data, x, y) {
+      this.gameProgress = 1;
+      if (this.buttonArr[x][y].isShow) return;
+      if (this.buttonArr[x][y].isSign) {
+        this.buttonArr[x][y].isSign = false;
+        this.signNum--;
+        let index = this.checkArr.findIndex(
+          (item) => item.index === data.index
+        );
+        this.checkArr.splice(index, 1);
+      } else {
+        this.checkArr.push(data);
+        this.signNum++;
+        this.buttonArr[x][y].isSign = true;
+      }
+    },
+    // 切换
+    changeFn(type) {
+      this.fnActiveName = type;
     },
   },
 };
 </script>
 <style lang="scss" scoped>
 .box {
-  width: 500px;
-  margin: 50px auto;
+  display: inline-block;
+  margin: 0 auto;
   padding: 5px;
   border: 5px solid black;
+  position: relative;
+  .progress {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 18px;
+    .succed {
+      color: rgb(255, 220, 0);
+    }
+    .error {
+      color: #000;
+    }
+  }
 }
 
 .list {
@@ -275,10 +295,13 @@ export default {
   height: 30px;
 }
 
-button {
+.list_row {
   width: 30px;
   height: 30px;
   background-color: #c0c0c0;
+  border: solid 1px #000;
+  text-align: center;
+  line-height: 2;
 }
 
 .bgc1 {
@@ -290,13 +313,13 @@ button {
 }
 
 .bgc3 {
-  background: url(./qq.jpg) no-repeat;
+  background: url(../static/sign-icon.png) no-repeat;
   background-color: #c0c0c0;
   background-size: 100%;
 }
 
 .active {
-  background: url(./Snipaste_2022-06-12_16-24-48.jpg) no-repeat;
+  background: url(../static/mine-icon.png) no-repeat;
   background-size: 100%;
 }
 
@@ -305,5 +328,55 @@ p {
   top: 200px;
   right: 200px;
   font-size: 20px;
+}
+.hide {
+  display: none;
+}
+.show {
+  display: block;
+}
+.lei1 {
+  color: rgb(3, 2, 238);
+}
+.lei2 {
+  color: rgb(15, 121, 13);
+}
+.lei3 {
+  color: rgb(215, 16, 16);
+}
+.lei4 {
+  color: rgb(2, 0, 126);
+}
+.lei5 {
+  color: rgb(120, 4, 4);
+}
+.lei6 {
+  color: rgb(6, 125, 126);
+}
+.mine_header {
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 24px;
+}
+.fn_box {
+  margin-top: 24px;
+  display: flex;
+  justify-content: space-evenly;
+  .fn {
+    padding: 5px 20px;
+    border-radius: 5px;
+    border: solid 1px #c0c0c0;
+  }
+  .fn:hover {
+    color: #1890ff;
+  }
+  .fn_active {
+    background: #1890ff;
+    border: solid 1px #1890ff;
+    color: #fff;
+  }
+  .fn_active:hover {
+    color: #fff;
+  }
 }
 </style>
